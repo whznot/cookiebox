675286168b1bfb772b1f4ee861b07c0eb2c7825d
whznot <vonmothur@gmail.com>
2024-10-28 16:57:14 +0100

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return(
+        <header>
+            <h1>cookiebox</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Servise</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr/>
+        </header>
+    );
+}
+
+export default Header
