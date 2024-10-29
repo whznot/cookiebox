1f8afae888441ecda59b64acf7920e2f5f40da3e
whznot <vonmothur@gmail.com>
2024-10-29 13:23:38 +0100

    4rd app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Button from './Button'
+import Student from "./Student";
 
 function App() {
-    return(
-        <>
-            <Button/>
-        </>
-    );
+  return (
+    <>
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidwart" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student/>
+    </>
+  );
 }
 
-export default App
+export default App;
